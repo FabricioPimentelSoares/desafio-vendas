c207d73d13ba67d53c4aae0967e1cd632b3eefc8
Fabricio <fabriciosoares.br@gmail.com>
2020-08-29 15:00:24 -0400

    Criando as tabelas

---
+++ database/migrations/2020_08_29_181857_create_item_vendas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_vendas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('venda_id');
+            $table->foreign('venda_id')->references('id')->on('vendas');
+            $table->unsignedBigInteger('produto_id');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->int('quantidade');
+            $table->decimal('valor_unitario', 10, 2);
+            $table->decimal('valot_total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_vendas');
+    }
+}
